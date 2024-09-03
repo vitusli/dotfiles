3b82810db6bc58204af6e5620574def18adee381
vitusli <vituspach@gmail.com>
2024-09-03 18:20:37 +0200

    daily

---
+++ sketchybar/.config/sketchybar/items/mic_status.sh
@@ -0,0 +1,3 @@
+sketchybar -m --add item mic right \
+  --set mic update_freq=1 \
+  --set mic script="~/.config/sketchybar/plugins/mic.sh"
--- sketchybar/.config/sketchybar/plugins/as_space.sh
+++ sketchybar/.config/sketchybar/plugins/as_space.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-echo \$FOCUSED_WORKSPACE: $FOCUSED_WORKSPACE, \$NAME: $NAME \$1: $1 >> ~/aaaa
+echo \$FOCUSED_WORKSPACE: $FOCUSED_WORKSPACE, \$NAME: $NAME \$1: $1 >>~/aaaa
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME background.drawing=on icon.highlight=off background.color=0x70000000
 else
-    sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME background.drawing=on icon.highlight=on background.color=0x20000000
 fi
+
