b0f12e800c51f3d778ec7d1c4086a9bccaad0630
vitusli <vituspach@gmail.com>
2026-01-21 09:32:34 +0100

    removed path duplicate

--- dot_zsh/path.zsh
+++ dot_zsh/path.zsh
@@ -2,8 +2,5 @@
 # PATH CONFIGURATION
 # ============================================================================
 
-# Local bin
-export PATH="$HOME/.local/bin:$PATH"
-
 # Rustup (Homebrew keg-only)
 export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
