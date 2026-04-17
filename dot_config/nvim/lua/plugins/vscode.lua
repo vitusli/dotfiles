a75726fe4dece0ba8f525534379335c0d6b12031
vitusli <vituspach@gmail.com>
2026-04-17 04:13:38 +0200

    this works fine ... typst issues

--- dot_config/nvim/lua/plugins/vscode.lua
+++ dot_config/nvim/lua/plugins/vscode.lua
@@ -2,45 +2,17 @@ if not vim.g.vscode then
   return {}
 end
 
-local function disable_vscode_rendering(args)
-  local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
-
-  vim.api.nvim_set_option_value("spell", false, { win = 0 })
-  vim.bo[args.buf].syntax = ""
-  pcall(vim.treesitter.stop, args.buf)
-
-  for _, client in ipairs(get_clients({ bufnr = args.buf })) do
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-end
-
 return {
   {
     "LazyVim/LazyVim",
     init = function()
-      vim.cmd("syntax off")
-      vim.diagnostic.enable(false)
-
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "FileType", "LspAttach", "WinEnter" }, {
-        group = vim.api.nvim_create_augroup("vscode_clean_rendering", { clear = true }),
-        callback = disable_vscode_rendering,
+      vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "FileType" }, {
+        group = vim.api.nvim_create_augroup("vscode_no_spell", { clear = true }),
+        pattern = "*",
+        callback = function()
+          vim.wo.spell = false
+        end,
       })
     end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    enabled = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    enabled = false,
-  },
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    enabled = false,
-  },
-  {
-    "folke/ts-comments.nvim",
-    enabled = false,
   },
 }
