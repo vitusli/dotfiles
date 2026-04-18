ea4fad431325444da91211455cb527fd2f791880
Vitus Pacholleck <vituspacholleck@Vitus-Work-Station.localdomain>
2026-04-18 21:44:16 +0200

    no more nvim stuff

--- dot_config/nvim/lua/plugins/extras.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.coding.mini-comment" },
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-}
