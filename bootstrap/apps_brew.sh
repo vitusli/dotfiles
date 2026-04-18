4cd2f220bf7ddaa8a368a7676406d7370ac9fa63
vitusli <vituspach@gmail.com>
2026-04-18 18:16:40 +0200

    Fix bootstrap: correct script filename and brew check

--- bootstrap/apps_brew.sh
+++ bootstrap/apps_brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-brew >/dev/null
+command -v brew >/dev/null
 { awk '/^#/||!NF{next}/#brew/{print "brew \""$1"\""}' "$(dirname "$0")/config/cli.txt" | { [[ "$(uname -s)" == "Linux" ]] && grep -Ev '^brew "git"$' || cat; }; [[ "$(uname -s)" == "Darwin" ]] && awk '/^#/||!NF{next}/#brew/{print "cask \""$1"\""}' "$(dirname "$0")/config/gui.txt"; } | brew bundle --file=-
 
 # git blacklisted on linux
